7164d41d137b480492998962f55762c934fd42e7
Rubytuesdaysandwich <rueben.durfee@gmail.com>
2022-05-10 20:36:22 -0600

    added comments to PHP first steps

--- helloworld.php
+++ helloworld.php
@@ -1,9 +1,19 @@
 <!DOCTYPE html PUBLIC>
 <html lang="en">
   <head>
-    <title>Document</title>
+    <title>Hello world</title>
   </head>
   <body>
-<?php echo"Hello World!";?>
+      <?php 
+      //single-line comments are like this
+      # or like this (less common)
+      /* double-line comments are written
+       like this, so that you can keep typing 
+       and typing
+       */
+      ?>
+<?php echo"Hello World! ";?><br/>
+<?php echo"Hello" . " World!";?><br/>
+<?php echo 2+3; ?>
   </body>
 </html>
